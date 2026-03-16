9df326dfc7f8f4e2355626c84a4f8a29d1ee7e08
Eduardo josé <eduardojos457@gmail.com>
2026-03-16 14:44:48 -0300

    o

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,14 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA60-0fcPA-WqYeCQaPuY2R3Fd365tzhkU",
-  authDomain: "nibuy-banco-de-dados.firebaseapp.com",
-  projectId: "nibuy-banco-de-dados",
-  storageBucket: "nibuy-banco-de-dados.firebasestorage.app",
-  messagingSenderId: "639899119190",
-  appId: "1:639899119190:web:230ef61b72086faeaed5f7",
-  measurementId: "G-Y17EDBZVTD"
+  // Mudamos de valores fixos para variáveis de ambiente do Vite
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
